1993dca9363338b630bda880481d76ec407e0bf7
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2024-05-02 02:38:50 -0300

    Alterada as funções
    Configurado o jeito certo de salvar o csv
    Alterada as funções de Tempo, Comparações, Memória, agora só repetir para os próximos testes este mesmo formato

--- lista_ligada_nao_ordenada.py
+++ lista_ligada_nao_ordenada.py
@@ -39,39 +39,41 @@ for arquivo in arquivos:
     #lista.sort(reverse=False)
 
     #Escolhe os piores casos para teste
-    pior_caso1 = -1
-    pior_caso2 = -50
-    pior_caso3 = -90
+    #pior_caso1 = -1
+    #pior_caso2 = -50
+    #pior_caso3 = -90
     #pior_caso3 = lista[-1] #funciona se a lista estiver ordenada
 
     '--------- Pior Cenário ---------'
     desvio = DesvioPadrao()
 
-    inicio_tempo_pior = medir_tempo()
-    inicio_memoria_pior = memoria_inicio()
+    casos_piores = [-1,-50,-90]
+    total_comparacoes_pior = []
+    tempos_pior = []
+    memoria_pior = []
 
-    lista_ligada.busca_sequencial_lista_ligada(valor=pior_caso1)
-    comparacoes_1 = lista_ligada.comparacoes_totais
+    for x in casos_piores:
+        inicio_tempo_pior = medir_tempo()
+        inicio_memoria_pior = memoria_inicio()
 
-    lista_ligada.comparacoes_totais = 0
-    lista_ligada.busca_sequencial_lista_ligada(valor=pior_caso2)
-    comparacoes_2 = lista_ligada.comparacoes_totais
+        lista_ligada.busca_sequencial_lista_ligada(valor=x)
+        total_comparacoes_pior.append(lista_ligada.comparacoes_totais)
 
-    lista_ligada.comparacoes_totais = 0
-    lista_ligada.busca_sequencial_lista_ligada(valor=pior_caso3)
-    comparacoes_3 = lista_ligada.comparacoes_totais
+        fim_tempo_pior = medir_tempo()
+        fim_memoria_pior = memoria_fim()
 
-    # Usando a classe DesvioPadrao
-    total_comparacoes_pior = [comparacoes_1, comparacoes_2, comparacoes_3]
+        memoria_pior.append(memoria_total_consumida(inicio=inicio_memoria_pior,fim=fim_memoria_pior))
+        tempos_pior.append((fim_tempo_pior - inicio_tempo_pior) * 1000)
+
+        lista_ligada.comparacoes_totais = 0
+        inicio_tempo_pior = 0
+        inicio_memoria_pior = 0
+
     desvio_padrao_resultado_pior = desvio.calcular(total_comparacoes_pior)
+    media_comparacoes_pior = desvio.media(total_comparacoes_pior)
 
-    fim_tempo_pior = medir_tempo()
-    fim_memoria_pior = memoria_fim()
-
-    tempo_pior = (fim_tempo_pior - inicio_tempo_pior) * 1000  # Convertendo para ms
-    memoria_consumida_pior = memoria_total_consumida(inicio=inicio_memoria_pior,fim=fim_memoria_pior)
-
-    # 100 números aleatórios da lista enviada
+    tempo_pior = sum(tempos_pior)
+    memoria_consumida_pior = sum(memoria_pior)
 
     '--------- 100 Números Aleatórios ---------'
 
@@ -81,23 +83,34 @@ for arquivo in arquivos:
     total_de_buscas_aleatorias = 100
     numeros_aleatorios = random.sample(lista, total_de_buscas_aleatorias)
 
-    inicio_tempo_aleatorio = medir_tempo()
-    inicio_memoria_aleatorio = memoria_inicio()
 
+    total_comparacoes_aleatorio = []
+    tempos_aleatorio = []
+    memoria_aleatorio = []
 
-    for x in numeros_aleatorios:
+    for x in numeros_aleatorios:    
+        inicio_tempo_aleatorio = medir_tempo()
+        inicio_memoria_aleatorio = memoria_inicio()
+
         lista_ligada.busca_sequencial_lista_ligada(valor=x)
-        comparacoes_aleatorio_lista.append(lista_ligada.comparacoes_totais)
+        total_comparacoes_aleatorio.append(lista_ligada.comparacoes_totais)
+
+        fim_tempo_aleatorio = medir_tempo()
+        fim_memoria_aleatorio = memoria_fim()
+
+        memoria_aleatorio.append(memoria_total_consumida(inicio=inicio_memoria_aleatorio,fim=fim_memoria_aleatorio))
+        tempos_aleatorio.append((fim_tempo_aleatorio - inicio_tempo_aleatorio) * 1000)
+
         lista_ligada.comparacoes_totais = 0
+        inicio_tempo_aleatorio = 0
+        inicio_memoria_aleatorio = 0
 
-    desvio_padrao_resultado_aleatorio = desvio.calcular(comparacoes_aleatorio_lista)
+    desvio_padrao_resultado_aleatorio = desvio.calcular(total_comparacoes_aleatorio)
+    media_comparacoes_aleatorio = desvio.media(total_comparacoes_aleatorio)
+    
+    tempo_aleatorio = sum(tempos_aleatorio)
+    memoria_consumida_aleatorio = sum(memoria_aleatorio)
 
-    fim_tempo_aleatorio = medir_tempo()
-    fim_memoria_aleatorio = memoria_fim()
-
-    tempo_aleatorio = (fim_tempo_aleatorio - inicio_tempo_aleatorio) * 1000  # Convertendo para ms
-    memoria_consumida_aleatorio = memoria_total_consumida(inicio=inicio_memoria_aleatorio, fim=fim_memoria_aleatorio)
-    total_comparacoes_aleatorio = sum(comparacoes_aleatorio_lista)
 
     # Salva os resultados em um arquivo CSV
     nome_arquivo_csv = 'lista_ligada_nao_ordenada.csv'
@@ -109,13 +122,16 @@ for arquivo in arquivos:
 
         # Escreve cabeçalhos apenas se o arquivo não existir
         if not arquivo_existe:
-            escritor_csv.writerow(["Arranjo", "Tipo de Teste", "Tempo (ms)", "Memória Consumida (bytes)", "Comparações","Desvio Padrão Comparações"])
+            escritor_csv.writerow(["Arranjo", "Tipo de Teste", "Média Tempo (ms)", "Desvio Pradrão Tempo (ms)", "Média Comparações","Desvio Padrão Comparações", "Média Memória (bytes)", "Desvio Padrão Memória (bytes)"])
 
         # Adiciona os dados dos testes
-        escritor_csv.writerow([tamanho_arranjo, "Pior Cenário", round(tempo_pior), memoria_consumida_pior, sum(total_comparacoes_pior), round(desvio_padrao_resultado_pior)])
-        escritor_csv.writerow([tamanho_arranjo, "100 Números Aleatórios", round(tempo_aleatorio), memoria_consumida_aleatorio, total_comparacoes_aleatorio, round(desvio_padrao_resultado_aleatorio)])
+        escritor_csv.writerow([tamanho_arranjo, "Pior Cenário", round(desvio.media(tempos_pior)), round(desvio.calcular(tempos_pior)), round(desvio.media(total_comparacoes_pior)), round(desvio.calcular(total_comparacoes_pior)), round(desvio.media(memoria_pior)),round(desvio.calcular(memoria_pior))])
+        escritor_csv.writerow([tamanho_arranjo, "100 Números Aleatórios", round(desvio.media(tempos_aleatorio)), round(desvio.calcular(tempos_aleatorio)), round(desvio.media(total_comparacoes_aleatorio)), round(desvio.calcular(total_comparacoes_aleatorio)), round(desvio.media(memoria_aleatorio)),round(desvio.calcular(memoria_aleatorio))])
 
     print("Novos resultados adicionados com sucesso no arquivo:", nome_arquivo_csv)
 
-    del memoria_consumida_aleatorio,memoria_consumida_pior,fim_memoria_aleatorio,fim_memoria_pior,inicio_memoria_aleatorio,inicio_memoria_pior
+    #del memoria_consumida_aleatorio,memoria_consumida_pior,fim_memoria_aleatorio,fim_memoria_pior,inicio_memoria_aleatorio,inicio_memoria_pior
+    del desvio, casos_piores, total_comparacoes_pior, tempos_pior, memoria_pior, comparacoes_aleatorio_lista, total_comparacoes_aleatorio, tempos_aleatorio, memoria_aleatorio, numeros_aleatorios
+
+
 print("Testes Finalizados!!")
