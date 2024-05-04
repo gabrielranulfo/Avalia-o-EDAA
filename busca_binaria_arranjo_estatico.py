a461be645b04f2a1d9514760c4767a9e9b2755f1
Gabriel Branquinho <gabrielranulfo14@gmail.com>
2024-05-04 18:46:18 -0300

    Testes realizados e resultados obtidos

--- busca_binaria_arranjo_estatico.py
+++ busca_binaria_arranjo_estatico.py
@@ -1,14 +1,15 @@
 import csv
 import random
 import os
-from algoritimos.lista_ligada import LinkedList
-from medidores.medidores import medir_tempo, get_pid, DesvioPadrao,memoria_fim,memoria_inicio,memoria_total_consumida
+from algoritimos.arranjo_estatico import ArranjoEstatico
+from medidores.medidores import medir_tempo, memoria_inicio, memoria_fim, memoria_total_consumida, DesvioPadrao, esperar
 
 def ler_arquivo(nome_arquivo):
     lista = []
     with open(nome_arquivo, "r") as arquivo_aberto:
         for number in arquivo_aberto:
-            lista.append(number.strip())
+            lista.append(int(number.strip()))  # Convertendo para inteiros
+    #lista.sort()
     return lista
 
 diretorio = './arquivos/'
@@ -17,28 +18,20 @@ arquivos = os.listdir(diretorio)
 for arquivo in arquivos:
     caminho_arquivo = os.path.join(diretorio, arquivo)
 
-    pid = get_pid()
-
     lista = ler_arquivo(caminho_arquivo)
     tamanho_arranjo = len(lista)
 
-    #instanciei
-    lista_ligada = LinkedList()
+    # Instanciando o arranjo estático
+    arranjo_estatico = ArranjoEstatico(tamanho_arranjo)
 
-    lista_ligada.comparacoes_totais = 0
-
-    # Preenche a lista ligada
-    for x in lista:
-        lista_ligada.append(x)
-
-    #ordena vetor chamado lista
-    #lista.sort(reverse=False)
+    # Preenche o arranjo estático
+    for i, valor in enumerate(lista):
+        arranjo_estatico.inserir(valor, i)
 
     '--------- Pior Cenário ---------'
     desvio = DesvioPadrao()
 
-    #casos_piores = [-1,-50,lista[-1]] #funciona só se a lista for ordenada
-    casos_piores = [-1,-50,-90]
+    casos_piores = [-1, -50, -90]
     total_comparacoes_pior = []
     tempos_pior = []
     memoria_pior = []
@@ -47,33 +40,25 @@ for arquivo in arquivos:
         inicio_tempo_pior = medir_tempo()
         inicio_memoria_pior = memoria_inicio()
 
-        lista_ligada.busca_sequencial_lista_ligada(valor=x,sort=False)
-        total_comparacoes_pior.append(lista_ligada.comparacoes_totais)
+        arranjo_estatico.buscar_binaria(x)
+        total_comparacoes_pior.append(arranjo_estatico.comparacoes_totais)
 
         fim_tempo_pior = medir_tempo()
         fim_memoria_pior = memoria_fim()
 
-        memoria_pior.append(memoria_total_consumida(inicio=inicio_memoria_pior,fim=fim_memoria_pior))
-        tempos_pior.append((fim_tempo_pior - inicio_tempo_pior) * 1000)
-
-        lista_ligada.comparacoes_totais = 0
-        inicio_tempo_pior = 0
-        inicio_memoria_pior = 0
+        memoria_pior.append(memoria_total_consumida(inicio=inicio_memoria_pior, fim=fim_memoria_pior))
+        tempos_pior.append((fim_tempo_pior - inicio_tempo_pior) * 1e6)
+        arranjo_estatico.comparacoes_totais = 0
 
     desvio_padrao_resultado_pior = desvio.calcular(total_comparacoes_pior)
     media_comparacoes_pior = desvio.media(total_comparacoes_pior)
 
-    tempo_pior = sum(tempos_pior)
-    memoria_consumida_pior = sum(memoria_pior)
-
     '--------- 100 Números Aleatórios ---------'
-
-    comparacoes_aleatorio_lista = []
+    comparacoes_aleatorio_arranjo = []
     desvio = DesvioPadrao()
-    lista_ligada.comparacoes_totais = 0
+    arranjo_estatico.comparacoes_totais = 0
     total_de_buscas_aleatorias = 100
     numeros_aleatorios = random.sample(lista, total_de_buscas_aleatorias)
-
 
     total_comparacoes_aleatorio = []
     tempos_aleatorio = []
@@ -83,16 +68,16 @@ for arquivo in arquivos:
         inicio_tempo_aleatorio = medir_tempo()
         inicio_memoria_aleatorio = memoria_inicio()
 
-        lista_ligada.busca_sequencial_lista_ligada(valor=x,sort=False)
-        total_comparacoes_aleatorio.append(lista_ligada.comparacoes_totais)
+        arranjo_estatico.buscar_binaria(x)
+        total_comparacoes_aleatorio.append(arranjo_estatico.comparacoes_totais)
 
         fim_tempo_aleatorio = medir_tempo()
         fim_memoria_aleatorio = memoria_fim()
 
         memoria_aleatorio.append(memoria_total_consumida(inicio=inicio_memoria_aleatorio,fim=fim_memoria_aleatorio))
-        tempos_aleatorio.append((fim_tempo_aleatorio - inicio_tempo_aleatorio) * 1000)
+        tempos_aleatorio.append((fim_tempo_aleatorio - inicio_tempo_aleatorio) * 1e6)
 
-        lista_ligada.comparacoes_totais = 0
+        arranjo_estatico.comparacoes_totais = 0
         inicio_tempo_aleatorio = 0
         inicio_memoria_aleatorio = 0
 
@@ -107,18 +92,14 @@ for arquivo in arquivos:
     if not os.path.exists(nome_diretorio):
         os.makedirs(nome_diretorio)
 
-    nome_arquivo_csv = os.path.join(nome_diretorio, 'lista_ligada_nao_ordenada.csv')
-
-    #nome_arquivo_csv = '/resultados/lista_ligada_nao_ordenada.csv'
-    # Verifica se o arquivo CSV já existe
-    arquivo_existe = os.path.isfile(nome_arquivo_csv)
+    nome_arquivo_csv = os.path.join(nome_diretorio, 'busca_binaria_arranjo_estatico_nao_ordenado.csv')
 
     with open(nome_arquivo_csv, mode='a', newline='', encoding='utf-8') as arquivo_csv:
         escritor_csv = csv.writer(arquivo_csv)
 
         # Escreve cabeçalhos apenas se o arquivo não existir
-        if not arquivo_existe:
-            escritor_csv.writerow(["Arranjo", "Tipo de Teste", "Média Tempo (ms)", "Desvio Pradrão Tempo (ms)", "Média Comparações","Desvio Padrão Comparações", "Média Memória (bytes)", "Desvio Padrão Memória (bytes)"])
+        if os.stat(nome_arquivo_csv).st_size == 0:
+            escritor_csv.writerow(["Arranjo", "Tipo de Teste", "Média Tempo (μs)", "Desvio Padrão Tempo (μs)", "Média Comparações","Desvio Padrão Comparações", "Média Memória (bytes)", "Desvio Padrão Memória (bytes)"])
 
         # Adiciona os dados dos testes
         escritor_csv.writerow([tamanho_arranjo, "Pior Cenário", round(desvio.media(tempos_pior)), round(desvio.calcular(tempos_pior)), round(desvio.media(total_comparacoes_pior)), round(desvio.calcular(total_comparacoes_pior)), round(desvio.media(memoria_pior)),round(desvio.calcular(memoria_pior))])
@@ -126,8 +107,7 @@ for arquivo in arquivos:
 
     print("Novos resultados adicionados com sucesso no arquivo:", nome_arquivo_csv)
 
-    #del memoria_consumida_aleatorio,memoria_consumida_pior,fim_memoria_aleatorio,fim_memoria_pior,inicio_memoria_aleatorio,inicio_memoria_pior
-    del desvio, casos_piores, total_comparacoes_pior, tempos_pior, memoria_pior, comparacoes_aleatorio_lista, total_comparacoes_aleatorio, tempos_aleatorio, memoria_aleatorio, numeros_aleatorios
-
+    # Liberando a memória
+    del desvio, casos_piores, total_comparacoes_pior, tempos_pior, memoria_pior, comparacoes_aleatorio_arranjo, total_comparacoes_aleatorio, tempos_aleatorio, memoria_aleatorio, numeros_aleatorios
 
 print("Testes Finalizados!!")
--- busca_sequencial_lista_ligada_nao_ordenada.py
+++ busca_sequencial_lista_ligada_nao_ordenada.py
@@ -1,15 +1,14 @@
 import csv
 import random
 import os
-from algoritimos.arranjo_estatico import ArranjoEstatico
-from medidores.medidores import medir_tempo, memoria_inicio, memoria_fim, memoria_total_consumida, DesvioPadrao, esperar
+from algoritimos.lista_ligada import LinkedList
+from medidores.medidores import medir_tempo, get_pid, DesvioPadrao,memoria_fim,memoria_inicio,memoria_total_consumida
 
 def ler_arquivo(nome_arquivo):
     lista = []
     with open(nome_arquivo, "r") as arquivo_aberto:
         for number in arquivo_aberto:
-            lista.append(int(number.strip()))  # Convertendo para inteiros
-    #lista.sort()
+            lista.append(number.strip())
     return lista
 
 diretorio = './arquivos/'
@@ -18,20 +17,28 @@ arquivos = os.listdir(diretorio)
 for arquivo in arquivos:
     caminho_arquivo = os.path.join(diretorio, arquivo)
 
+    pid = get_pid()
+
     lista = ler_arquivo(caminho_arquivo)
     tamanho_arranjo = len(lista)
 
-    # Instanciando o arranjo estático
-    arranjo_estatico = ArranjoEstatico(tamanho_arranjo)
+    #instanciei
+    lista_ligada = LinkedList()
 
-    # Preenche o arranjo estático
-    for i, valor in enumerate(lista):
-        arranjo_estatico.inserir(valor, i)
+    lista_ligada.comparacoes_totais = 0
+
+    # Preenche a lista ligada
+    for x in lista:
+        lista_ligada.append(x)
+
+    #ordena vetor chamado lista
+    #lista.sort(reverse=False)
 
     '--------- Pior Cenário ---------'
     desvio = DesvioPadrao()
 
-    casos_piores = [-1, -50, -90]
+    #casos_piores = [-1,-50,lista[-1]] #funciona só se a lista for ordenada
+    casos_piores = [-1,-50,-90]
     total_comparacoes_pior = []
     tempos_pior = []
     memoria_pior = []
@@ -40,25 +47,33 @@ for arquivo in arquivos:
         inicio_tempo_pior = medir_tempo()
         inicio_memoria_pior = memoria_inicio()
 
-        arranjo_estatico.buscar_sequencial(x)
-        total_comparacoes_pior.append(arranjo_estatico.comparacoes_totais)
+        lista_ligada.busca_sequencial_lista_ligada(valor=x,sort=False)
+        total_comparacoes_pior.append(lista_ligada.comparacoes_totais)
 
         fim_tempo_pior = medir_tempo()
         fim_memoria_pior = memoria_fim()
 
-        memoria_pior.append(memoria_total_consumida(inicio=inicio_memoria_pior, fim=fim_memoria_pior))
+        memoria_pior.append(memoria_total_consumida(inicio=inicio_memoria_pior,fim=fim_memoria_pior))
         tempos_pior.append((fim_tempo_pior - inicio_tempo_pior) * 1e6)
-        arranjo_estatico.comparacoes_totais = 0
+
+        lista_ligada.comparacoes_totais = 0
+        inicio_tempo_pior = 0
+        inicio_memoria_pior = 0
 
     desvio_padrao_resultado_pior = desvio.calcular(total_comparacoes_pior)
     media_comparacoes_pior = desvio.media(total_comparacoes_pior)
 
+    tempo_pior = sum(tempos_pior)
+    memoria_consumida_pior = sum(memoria_pior)
+
     '--------- 100 Números Aleatórios ---------'
-    comparacoes_aleatorio_arranjo = []
+
+    comparacoes_aleatorio_lista = []
     desvio = DesvioPadrao()
-    arranjo_estatico.comparacoes_totais = 0
+    lista_ligada.comparacoes_totais = 0
     total_de_buscas_aleatorias = 100
     numeros_aleatorios = random.sample(lista, total_de_buscas_aleatorias)
+
 
     total_comparacoes_aleatorio = []
     tempos_aleatorio = []
@@ -68,8 +83,8 @@ for arquivo in arquivos:
         inicio_tempo_aleatorio = medir_tempo()
         inicio_memoria_aleatorio = memoria_inicio()
 
-        arranjo_estatico.buscar_sequencial(x)
-        total_comparacoes_aleatorio.append(arranjo_estatico.comparacoes_totais)
+        lista_ligada.busca_sequencial_lista_ligada(valor=x,sort=False)
+        total_comparacoes_aleatorio.append(lista_ligada.comparacoes_totais)
 
         fim_tempo_aleatorio = medir_tempo()
         fim_memoria_aleatorio = memoria_fim()
@@ -77,7 +92,7 @@ for arquivo in arquivos:
         memoria_aleatorio.append(memoria_total_consumida(inicio=inicio_memoria_aleatorio,fim=fim_memoria_aleatorio))
         tempos_aleatorio.append((fim_tempo_aleatorio - inicio_tempo_aleatorio) * 1e6)
 
-        arranjo_estatico.comparacoes_totais = 0
+        lista_ligada.comparacoes_totais = 0
         inicio_tempo_aleatorio = 0
         inicio_memoria_aleatorio = 0
 
@@ -92,14 +107,18 @@ for arquivo in arquivos:
     if not os.path.exists(nome_diretorio):
         os.makedirs(nome_diretorio)
 
-    nome_arquivo_csv = os.path.join(nome_diretorio, 'busca_sequencial_arranjo_estatico_nao_ordenado.csv')
+    nome_arquivo_csv = os.path.join(nome_diretorio, 'lista_ligada_nao_ordenada1e6.csv')
+
+    #nome_arquivo_csv = '/resultados/lista_ligada_nao_ordenada.csv'
+    # Verifica se o arquivo CSV já existe
+    arquivo_existe = os.path.isfile(nome_arquivo_csv)
 
     with open(nome_arquivo_csv, mode='a', newline='', encoding='utf-8') as arquivo_csv:
         escritor_csv = csv.writer(arquivo_csv)
 
         # Escreve cabeçalhos apenas se o arquivo não existir
-        if os.stat(nome_arquivo_csv).st_size == 0:
-            escritor_csv.writerow(["Arranjo", "Tipo de Teste", "Média Tempo (μs)", "Desvio Padrão Tempo (μs)", "Média Comparações","Desvio Padrão Comparações", "Média Memória (bytes)", "Desvio Padrão Memória (bytes)"])
+        if not arquivo_existe:
+            escritor_csv.writerow(["Arranjo", "Tipo de Teste", "Média Tempo (μs)", "Desvio Pradrão Tempo (μs)", "Média Comparações","Desvio Padrão Comparações", "Média Memória (bytes)", "Desvio Padrão Memória (bytes)"])
 
         # Adiciona os dados dos testes
         escritor_csv.writerow([tamanho_arranjo, "Pior Cenário", round(desvio.media(tempos_pior)), round(desvio.calcular(tempos_pior)), round(desvio.media(total_comparacoes_pior)), round(desvio.calcular(total_comparacoes_pior)), round(desvio.media(memoria_pior)),round(desvio.calcular(memoria_pior))])
@@ -107,7 +126,8 @@ for arquivo in arquivos:
 
     print("Novos resultados adicionados com sucesso no arquivo:", nome_arquivo_csv)
 
-    # Liberando a memória
-    del desvio, casos_piores, total_comparacoes_pior, tempos_pior, memoria_pior, comparacoes_aleatorio_arranjo, total_comparacoes_aleatorio, tempos_aleatorio, memoria_aleatorio, numeros_aleatorios
+    #del memoria_consumida_aleatorio,memoria_consumida_pior,fim_memoria_aleatorio,fim_memoria_pior,inicio_memoria_aleatorio,inicio_memoria_pior
+    del desvio, casos_piores, total_comparacoes_pior, tempos_pior, memoria_pior, comparacoes_aleatorio_lista, total_comparacoes_aleatorio, tempos_aleatorio, memoria_aleatorio, numeros_aleatorios
+
 
 print("Testes Finalizados!!")
